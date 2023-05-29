5285f8108d2e869d3b09da067cd1b7ce5480353c
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-05-29 16:45:58 -0300

    Add calculator logic to component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,28 @@
+import React, { useState } from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 import ChildComponent from './ChildComponent';
 
-const Calculator = () => (
-  <div className="container">
-    <ChildComponent />
-  </div>
-);
+const Calculator = () => {
+  const [calcData, setCalcData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    const newData = calculate(calcData, buttonName);
+    setCalcData(newData);
+  };
+
+  return (
+    <div className="container">
+      <ChildComponent
+        calcData={calcData}
+        handleButtonClick={handleButtonClick}
+      />
+    </div>
+  );
+};
 
 export default Calculator;
